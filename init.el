5b8eac44263eb6711663e5bdd9a2ab9df80bbce0
Quest <quest@quest-it.eu>
2020-08-25 13:18:50 +0200

    added submodules and started configuring emacs

--- init.el
+++ init.el
@@ -1,11 +1,3 @@
-;; -*- mode: elisp -*-
-
-;; Disable the splash screen (to enable it agin, replace the t with 0)
-(setq inhibit-splash-screen t)
-
-;; Enable transient mark mode
-(transient-mark-mode 1)
-
 ;; Load Settings file
 (require 'org)
 (org-babel-load-file
