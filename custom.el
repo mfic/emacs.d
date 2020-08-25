d4d61b8cbe917fedc50e1d0740bf9e3edff43e37
Quest <quest@quest-it.eu>
2020-08-25 16:51:00 +0200

    added missing )

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("a624baa81c72ce6c99164190896cca78e36a609e3748054501d6116b973b2d9b" default))
+   '("a624baa81c72ce6c99164190896cca78e36a609e3748054501d6116b973b2d9b" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
