c85e91101895d8f236fb3e797ee2f6047077fe1f
Quest <quest@quest-it.eu>
2020-08-25 15:26:49 +0200

    added appearance and sane-defaults

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(##)))
+ '(custom-safe-themes
+   '("a624baa81c72ce6c99164190896cca78e36a609e3748054501d6116b973b2d9b" default))
+ '(package-selected-packages '(magit ##)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
