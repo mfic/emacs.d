d41770b77a5d7d1eb7eddcbf735323fc27617c48
Quest <quest@quest-it.eu>
2020-08-25 16:44:24 +0200

    restructured settings file

--- custom.el
+++ custom.el
@@ -5,7 +5,6 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("a624baa81c72ce6c99164190896cca78e36a609e3748054501d6116b973b2d9b" default))
- '(package-selected-packages '(magit ##)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
